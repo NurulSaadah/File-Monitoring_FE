61a3a77e86f19c59c20822e17578b8fa37de1da6
MOHDHAZIZI <82504629+MOHDHAZIZI@users.noreply.github.com>
2023-07-28 11:02:58 +0800

    login

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -12,7 +12,7 @@
           <va-tabs v-model="tabIndex" center>
             <template #tabs>
               <va-tab name="login">Login</va-tab>
-              <va-tab name="signup">Create New Account</va-tab>
+              <!--<va-tab name="signup">Create New Account</va-tab>-->
             </template>
           </va-tabs>
 
