89a43705d2a39ae7fa4dacf9cb3ee34e97764cb1
f41zf4u21 <f41zf4u21@gmail.com>
2023-08-02 10:52:08 +0800

    FM-7

--- src/pages/setting/user-management.vue
+++ src/pages/setting/user-management.vue
@@ -12,18 +12,32 @@
         <br />
         <div class="grid grid-cols-12 gap-6">
           <fieldset class="md:col-span-12 col-span-12 grid grid-cols-3 md:grid-cols-3 gap-3">
-            <va-checkbox v-model="checkbox.unselected1" label="Dashboard (m1)" value="m1" id="dashboard" @click="onClickScreenAccess('m1')"/>
-            <va-checkbox v-model="checkbox.unselected2" label="Tender Management (m2)" value="m2" id="tender" @click="onClickScreenAccess('m2')"/>
-            <va-checkbox v-model="checkbox.unselected3" label="CV Management (m3)" value="m3" id="cvmanagement" @click="onClickScreenAccess('m3')"/>
-            <va-checkbox v-model="checkbox.unselected4" label="My Backup files (m4)" value="m4" id="backup" @click="onClickScreenAccess('m4')"/>
-            <va-checkbox v-model="checkbox.unselected5" label="My CV (m5)" value="m5" id="mycv" @click="onClickScreenAccess('m5')"/>
-            <va-checkbox v-model="checkbox.unselected6" label="Settings (m6)" value="m6" id="settings" @click="onClickScreenAccess('m6')"/>
+            <va-checkbox v-model="m1" label="Dashboard (m1)" value="m1" id="dashboard" @click="onClickScreenAccess('m1')"/>
+            <va-checkbox v-model="m2" label="Tender Management (m2)" value="m2" id="tender" @click="onClickScreenAccess('m2')"/>
+            <va-checkbox v-model="m3" label="CV Management (m3)" value="m3" id="cvmanagement" @click="onClickScreenAccess('m3')"/>
+            <va-checkbox v-model="m4" label="My Backup files (m4)" value="m4" id="backup" @click="onClickScreenAccess('m4')"/>
+            <va-checkbox v-model="m5" label="My CV (m5)" value="m5" id="mycv" @click="onClickScreenAccess('m5')"/>
+            <va-checkbox v-model="m6" label="Settings (m6)" value="m6" id="settings" @click="onClickScreenAccess('m6')"/>
           </fieldset>
         </div>
       </form>
+      <div class="mt-5"></div>
+      <div class="grid grid-cols-12 gap-6">
+          <div class="flex md:col-span-4 sm:col-span-6 col-span-12">
+            <select style="border-radius: 5px; border: 1px solid lightgrey;width: 30%;height: 130%;"
+                  v-model="userStatus"
+                  class="form-select"
+                  aria-label="Default select example" label="Status">
+                    <option value="" disabled selected>Status</option>
+                    <option value="1">Active</option>
+                    <option value="0">Inactive</option>
+
+                 </select>
+          </div>
+        </div>
     </va-card-content>
     <va-card-content class="my-3 flex flex-wrap items-center gap-2">
-      <va-button icon="add_circle_outline" @click="onCreate()"> Save Record</va-button>
+      <va-button icon="add_circle_outline" @click="onCreate()"><Loader v-if="loader" /> Save Record</va-button>
     </va-card-content>
   </va-card>
   <br />
@@ -33,6 +47,7 @@
       <table class="va-table va-table--striped va-table--hoverable w-full">
         <thead>
           <tr>
+            <th>No</th>
             <th>Email</th>
             <th>Screen Access</th>
             <th>Status</th>
@@ -41,10 +56,11 @@
         </thead>
 
         <tbody>
-          <tr  v-for="user in userlist">
+          <tr  v-for="(user, idx) in userlist" :key="idx">
+            <td>{{ idx + 1 }}</td>
             <td>
                       <a
-                        style="color: #18846f; cursor: pointer"
+                        style="color: #18846f; cursor: default " 
                         >{{ user.email }}</a
                       >
             </td>
@@ -52,7 +68,7 @@
             <td v-if="user.status == 1">Active</td>  
             <td v-if="user.status == 0">Inactive</td>  
             <td> <va-list-item-section icon>
-              <va-icon name="edit" color="gray" @click="editModal = !editModal"/>
+              <va-icon name="edit" color="gray" @click="editRecord(user)"/>
               </va-list-item-section>
             </td>
           </tr>
@@ -61,80 +77,29 @@
     </va-card-content>
   </va-card>
 
-  <!-- modal -->
-  <va-modal
-    v-slot="{ hide }"
-    v-model="editModal"
-    message=""
-    hide-default-actions
-    blur
-  >
-  <div class="flex flex-col items-start gap-2">
-      <h3 class="va-h3">
-        Edit User
-      </h3>
-
-      <div class="flex md:col-span-4 sm:col-span-6 col-span-12">
-            <va-input v-model="modalEmail" placeholder="Enter Email (e.g: abc@araken.biz)" disabled/>
-      </div>
-
-      <div class="grid grid-cols-12 gap-6">
-          <fieldset class="md:col-span-12 col-span-12 grid grid-cols-3 md:grid-cols-3 gap-3">
-            <va-checkbox v-model="checkbox.unselected1" label="Dashboard (m1)" value="m1" id="dashboard" @click="onClickScreenAccess('m1')"/>
-            <va-checkbox v-model="checkbox.unselected2" label="Tender Management (m2)" value="m2" id="tender" @click="onClickScreenAccess('m2')"/>
-            <va-checkbox v-model="checkbox.unselected3" label="CV Management (m3)" value="m3" id="cvmanagement" @click="onClickScreenAccess('m3')"/>
-            <va-checkbox v-model="checkbox.unselected4" label="My Backup files (m4)" value="m4" id="backup" @click="onClickScreenAccess('m4')"/>
-            <va-checkbox v-model="checkbox.unselected5" label="My CV (m5)" value="m5" id="mycv" @click="onClickScreenAccess('m5')"/>
-            <va-checkbox v-model="checkbox.unselected6" label="Settings (m6)" value="m6" id="settings" @click="onClickScreenAccess('m6')"/>
-          </fieldset>
-      </div>
-
-    </div>
-
-    <div class="flex justify-end mt-2 gap-2">
-      <va-button
-        preset="secondary"
-        color="secondary"
-        @click="hide()"
-      >
-        Cancel
-      </va-button>
-      <va-button
-        @click="showSecondModal = !showSecondModal"
-      >
-        Save
-      </va-button>
-    </div>
-  </va-modal>
-
-  <!-- <va-modal
-    v-model="showSecondModal"
-    message="Are you sure you want to save it?"
-    ok-text="Save"
-    @ok="editModal = false"
-  /> -->
 </template>
 
 <script>
 import swal from 'sweetalert2';
-export default {
+import Loader from "../../components/loader.vue";
 
+export default {
+  components: { Loader },
   data() {
 
     return {
+      loader:false,
       userdetails: null,
       userlist : [],
       email: "",
-      checkbox:  [
-        {unselected1: false},
-        {unselected2: false},
-        {unselected3: false},
-        {unselected4: false},
-        {unselected5: false},
-        {unselected6: false}
-      ],
+      m1: false,
+      m2: false,
+      m3: false,
+      m4: false,
+      m5: false,
+      m6: false,
+      userStatus: "",
       screenaccess: "",
-      editModal: false,
       showSecondModal: false,
       modalEmail: "",
     };
@@ -174,6 +139,7 @@ export default {
                 }).then(async (result) => {
                 if (result.isConfirmed) {
                   try {
+                    this.loader = true;
                     const headers = {
                       Authorization: "Bearer " + this.userdetails.authorization.token,
                       Accept: "application/json",
@@ -184,17 +150,17 @@ export default {
                         {
                           email: this.email,
                           user_access: this.screenaccess,
-                          status: 1,
+                          status: this.userStatus,
                         }, {headers}
                       );
                       console.log("response", response.data);
                       if (response.data.code == 200) {
-                          //this.loader = false;
+                          this.loader = false;
                           this.resetModel();
                           swal.fire('Record Successfully Saved', '', 'success')
                           this.GetUserDetails();
                       } else {
-                          //this.loader = false;
+                          this.loader = false;
                           this.resetModel();
                           swal.fire({
                               icon: 'error',
@@ -217,8 +183,15 @@ export default {
     },
 
     resetModel(){
-      this.email='',
-      this.checkbox='';
+      this.email='';
+      this.m1 = false;
+      this.m2 = false;
+      this.m3 = false;
+      this.m4 = false;
+      this.m5 = false;
+      this.m6 = false;
+      this.screenaccess = "";
+      this.userStatus = "";
     },
 
     onClickScreenAccess(val){
@@ -274,6 +247,49 @@ export default {
 
       return sortedString;
     },
+
+    editRecord(data){
+      this.resetModel();
+      var mArray;
+
+      this.email = data.email;
+      this.userStatus = data.status;
+
+      mArray = data.user_access.split(',');
+
+      for(let i = 0; i< mArray.length; i++){
+        if(mArray[i] == 'm1'){
+          this.m1 = true
+        }
+        if(mArray[i] == 'm2'){
+          this.m2 = true
+        }
+        if(mArray[i] == 'm3'){
+          this.m3 = true
+        }
+        if(mArray[i] == 'm4'){
+          this.m4 = true
+        }
+        if(mArray[i] == 'm5'){
+          this.m5 = true
+        }
+        if(mArray[i] == 'm6'){
+          this.m6 = true
+        };
+      };
+
+      this.screenaccess = data.user_access;
+
+      this.scrollToTop();
+
+    },
+
+    scrollToTop() {
+      window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        });
+    }
   },
 }
 </script>
